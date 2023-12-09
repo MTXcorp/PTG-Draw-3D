0a3c966b9781fbd0dbc7557646a566454aeb554e
KKK (Klu Kli Klan) <152071748+MTXcorp@users.noreply.github.com>
2023-12-09 11:13:23 +0100

    Made changes, defined the Load_file Function, added if to the input1 variable

--- Pre-Releases/Early Access/PTG-Draw-3D-1.0.2.2-B.py
+++ Pre-Releases/Early Access/PTG-Draw-3D-1.0.2.2-B.py
@@ -1,6 +1,8 @@
 import turtle as t
 import os
 #Defining the Menus
+def Load_file():
+    print("load a")
 def main_menu_file():
     os.system("cls")
     print("PTG Draw 3D")
@@ -9,6 +11,11 @@ def main_menu_file():
     print("3 = Check Version")
     print("4 = Exit")
     Input1 = input("|>")
+    if Input1 == "1":
+        Set_name()
+    if Input1 == "2":
+        Load_file()
+
 def Set_name():
     os.system("cls")
     print("Name Of Drawing?")
@@ -18,3 +25,4 @@ def main_menu_shape():
     os.system("cls")
     print(name + " - PTG Draw 3D")
     print("1 = Add A Shape")
+    print("2 = Delete Shapes")
